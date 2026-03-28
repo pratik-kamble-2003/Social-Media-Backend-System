7ec8fa0c4c24af7be112667efcf3e987491d4d99
pratik-kamble <pratikvk2003@gmail.com>
2026-03-29 00:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get("/posts/:id/edit", (req, res) => {
     res.render("edit.ejs", { post });
 })
 
-app.delete("/post/:id", (req, res) => {
+app.delete("/posts/:id", (req, res) => {
     let { id } = req.params;
     posts = posts.filter((p) => id !== p.id);
     res.redirect("/posts");
